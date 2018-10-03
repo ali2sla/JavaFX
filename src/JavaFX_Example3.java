8c1e2780f0f9df3a38f4a1644bb2b9b347b3adac
vhernandez <vhernandez@scienceleadership.org>
2018-10-03 01:57:43 -0400

    Add JavaFX_Example3 which displays both text and button.

--- src/JavaFX_Example3.java
+++ src/JavaFX_Example3.java
@@ -3,13 +3,16 @@ package org.sla;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
-public class JavaFX_Example2 extends Application {
+public class JavaFX_Example3 extends Application {
     // Fields
     private Button clickMeButton;
     private int numberOfClicks = 0;
+    private Label myLabel;
 
     // Methods
     private void clickMeButton_click() {
@@ -18,6 +21,7 @@ public class JavaFX_Example2 extends Application {
         System.out.println("You clicked the ClickME button " + numberOfClicks + " times.");
         // Update the text in the button to show click count
         clickMeButton.setText("Click Me " + numberOfClicks);
+        myLabel.setText("HELLO " + numberOfClicks);
     }
 
     @Override
@@ -30,12 +34,17 @@ public class JavaFX_Example2 extends Application {
         // tell button what method will get called when it is clicked
         clickMeButton.setOnAction(e->clickMeButton_click());
 
-        // make a frame and add only the button UI element to it
-        StackPane myUIFrame = new StackPane();
-        myUIFrame.getChildren().add(clickMeButton);
+        // Create text UI element
+        myLabel = new Label("Hello Students!  This is Mr. Hernandez.");
+
+        // make a layout and add both button and text to it
+        HBox myBoxLayout = new HBox();
+        //VBox myBoxLayout = new VBox();
+        myBoxLayout.getChildren().add(clickMeButton);
+        myBoxLayout.getChildren().add(myLabel);
 
         // Fill 400x200 window with just that frame of UI Elements
-        Scene scene = new Scene(myUIFrame, 400, 200);
+        Scene scene = new Scene(myBoxLayout, 400, 200);
         primaryStage.setScene(scene);
 
         // Display the window
